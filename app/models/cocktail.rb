eefe46332893e1bfe1cd9b60e0e5917261e5d1a3
Zachary Goodnough <goodnough.z@gmail.com>
2019-07-26 14:49:14 +0800

    working on uploader

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,4 +2,6 @@ class Cocktail < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
+  mount_uploader :photo, PhotoUploader
+
 end
---
+++ db/migrate/20190726064057_add_photo_to.rb
@@ -0,0 +1,4 @@
+class AddPhotoTo < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
